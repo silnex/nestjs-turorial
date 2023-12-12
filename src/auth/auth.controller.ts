d642b3253993969b872e68dcfeb1f7095e3052ef
silnex <silnex@silnex.kr>
2023-12-12 23:24:25 +0800

    Install Prisma

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { Request } from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup() {
+  signup(@Body() body: any) {
     return this.authService.signup();
   }
 
